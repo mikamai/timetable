4cb2c87b3db044b5e449fb4c9499c9fa35309fc1
Nicola Racco <nicola@mikamai.com>
2017-12-15 12:47:36 +0100

    use right host in mails

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,12 +1,14 @@
-return if ENV['SENDGRID_USERNAME'].nil? || ENV['SENDGRID_PASSWORD'].nil?
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: Rails.application.secrets.web_domain }
+
+  return if ENV['SENDGRID_USERNAME'].nil? || ENV['SENDGRID_PASSWORD'].nil?
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
+    address:              'smtp.sendgrid.net',
+    port:                 587,
+    authentication:       :plain,
+    enable_starttls_auto: true
   }
 end
