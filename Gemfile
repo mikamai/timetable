738becb4ba639124cc8e99ff2bd98616188ca0c8
Nicola Racco <nicola@mikamai.com>
2017-12-19 16:04:00 +0100

    client specs

--- Gemfile
+++ Gemfile
@@ -68,5 +68,10 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+group :test do
+  gem 'database_cleaner'
+  gem 'rspec-collection_matchers'
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -3,5 +3,34 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'require a name' do
+      expect(subject).to have(1).error_on :name
+      subject.name = 'asd'
+      expect(subject).to have(0).errors_on :name
+    end
+
+    it 'require a unique name against the belonging organization' do
+      c = create :client, name: 'asd'
+      subject.organization = c.organization
+      subject.name = 'asd'
+      expect(subject).to have(1).error_on :name
+      subject.organization = build :organization
+      expect(subject).to have(0).error_on :name
+    end
+
+    it 'require an organization' do
+      expect(subject).to have(1).error_on :organization
+      subject.organization = build :organization
+      expect(subject).to have(0).error_on :organization
+    end
+  end
+
+  it 'cannot be destroyed if it is referenced in any project' do
+    p = create :project
+    subject = p.client
+    expect(subject.destroy).to be_falsey
+    p.destroy
+    expect(subject.destroy).to be_truthy
+  end
 end
