0b92696532ebf039849d78a4e699223f50bf84da
Nicola Racco <nicola@mikamai.com>
2018-02-19 14:43:56 +0100

    Revert "do not force setting aws credentials"
    This reverts commit 044c7d07e2c82d77b0d4a1811c8011f61034a181.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,9 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
-      region:                ENV.fetch('AWS_REGION', 'eu-west-1')
+      region:                ENV.fetch('AWS_REGION', 'eu-west-1'),
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     }
     config.fog_directory = ENV['S3_BUCKET']
     config.fog_public = false
