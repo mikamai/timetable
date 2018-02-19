044c7d07e2c82d77b0d4a1811c8011f61034a181
Nicola Racco <nicola@mikamai.com>
2018-02-19 14:35:08 +0100

    do not force setting aws credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,7 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
-      region:                ENV.fetch('AWS_REGION', 'eu-west-1'),
-      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region:                ENV.fetch('AWS_REGION', 'eu-west-1')
     }
     config.fog_directory = ENV['S3_BUCKET']
     config.fog_public = false
