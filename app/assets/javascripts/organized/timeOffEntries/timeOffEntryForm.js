0fa7dab07248824cf73a364922403bfe01b528bc
massierc <massimo.ercolani28@gmail.com>
2018-12-05 15:24:33 +0100

    add hidden field typology to time_off_periods and time_off_entries forms

--- app/assets/javascripts/organized/timeOffEntries/timeOffEntryForm.js
+++ app/assets/javascripts/organized/timeOffEntries/timeOffEntryForm.js
@@ -3,20 +3,31 @@
 $(() => {
   $('select#time_off_typology').change(e => {
     const val = e.target.value;
+
     const isPaidLeave = val => val.toLowerCase() === 'paid leave';
-    const isVacationOrSickLeave = val =>
-      ['vacation', 'sick leave'].includes(val.toLowerCase());
+    const isVacation = val => val.toLowerCase() === 'vacation';
+    const isSickLeave = val => val.toLowerCase() === 'sick leave';
+
     const hide = type => $(`#new_time_off_${type}`).addClass('d-none');
     const show = type => $(`#new_time_off_${type}`).removeClass('d-none');
+    const set = (type, typology) =>
+      $(`#time_off_${type}_typology`).val(typology);
 
     switch (true) {
       case isPaidLeave(val):
         hide('period');
         show('entry');
+        set('entry', 'paid');
         break;
-      case isVacationOrSickLeave(val):
+      case isVacation(val):
         hide('entry');
         show('period');
+        set('period', 'vacation');
+        break;
+      case isSickLeave():
+        hide('entry');
+        show('period');
+        set('period', 'sick');
         break;
       default:
         hide('period');
