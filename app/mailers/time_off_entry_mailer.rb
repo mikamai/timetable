4a9a04f3b485f824b1aa8d97ce18e57a8c12bf92
massierc <massimo.ercolani28@gmail.com>
2018-12-17 12:49:36 +0100

    create TimeOffEntryMailer and associated request_time_off view

---
+++ app/mailers/time_off_entry_mailer.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class TimeOffEntryMailer < ApplicationMailer
+  add_template_helper TimeEntriesHelper
+
+  def request_time_off request
+    @request = request
+    mail to: 'HR@mikamai.com', subject: "[Timetable] New #{@request.friendly_tipology} request from #{@request.user.name}"
+  end
+end
