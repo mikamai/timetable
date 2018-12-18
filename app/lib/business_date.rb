c61fe4981007f9fa005d8a7994c0d1e3e3f8000a
massierc <massimo.ercolani28@gmail.com>
2018-12-18 17:30:01 +0100

    parse date if params provided are strings

--- app/lib/business_date.rb
+++ app/lib/business_date.rb
@@ -1,5 +1,7 @@
 module BusinessDate
   def business_dates_between start_date, end_date
+    start_date = Date.parse(start_date) if start_date.class == String
+    end_date = Date.parse(end_date) if end_date.class == String
     (start_date..end_date).to_a.reject &is_weekend
   end
 
