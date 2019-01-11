5031868ebab3d42ee1496efba0330b2999ae561c
massierc <massimo.ercolani28@gmail.com>
2019-01-11 14:26:48 +0100

    use rails' on_weekend? method instead of creating my own

--- app/lib/business_date.rb
+++ app/lib/business_date.rb
@@ -2,12 +2,8 @@ module BusinessDate
   def business_dates_between start_date, end_date
     start_date = Date.parse(start_date) if start_date.class == String
     end_date = Date.parse(end_date) if end_date.class == String
-    (start_date..end_date).to_a.reject &is_weekend
+    (start_date..end_date).to_a.reject &:on_weekend?
   rescue
     nil
-  end
-
-  def is_weekend
-    -> (date) { (date.wday % 7 == 0) || (date.wday % 7 == 6) }
   end
 end
